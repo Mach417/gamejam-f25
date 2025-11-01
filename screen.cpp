46635e1686ce0867683b0daf21f46dad104e70ea
Mach417 <mckennedy417@gmail.com>
2025-10-31 21:21:05 -0400

    Finally working compilation on Windows with basic raylib 5.5 installation; unconfirmed on Linux or web

---
+++ screen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mcken on 10/31/2025.
+//
+
+#include "screen.h"
---
+++ screen.h
@@ -0,0 +1,16 @@
+//
+// Created by mcken on 10/31/2025.
+//
+
+#ifndef SCREEN_H
+#define SCREEN_H
+
+
+
+class screen {
+
+};
+
+
+
+#endif //SCREEN_H
